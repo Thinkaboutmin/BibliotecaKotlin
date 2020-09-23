078bd02003f1eee08d1722e8befef45668e0273b
StephanieAVT <stephanie.torisu08@gmail.com>
2020-09-23 17:15:23 -0300

    alteração 3 Stephanie

--- src/com/br/digitalhouse/Colecao.kt
+++ src/com/br/digitalhouse/Colecao.kt
@@ -2,28 +2,17 @@ package com.br.digitalhouse
 
 data class Colecao(val lista:MutableList<Livro>,val codigo:Int,val preco:Double,val descricao:String) {
 
-    fun verificarColecao():String{
+    fun validar():String{
         if (this.lista.isEmpty()) {
             return "A lista está vazia"
-        } else
-            return ""
-
-        if (this.codigo < 0){
+        } else if (this.codigo < 0){
             return "Codigo inválido"
-        } else
-            return ""
-
-        if (this.preco < 0) {
+        } else if (this.preco < 0) {
             return "O preco está negativo"
-        } else
-        return ""
-
-        if (this.descricao.isEmpty()){
+        } else if (this.descricao.isEmpty()){
             return "A descrição está vazia"
         }else
             return ""
-
     }
-
 
 }
