0551508a4eba3e046d07d1bcb5f949f200d6180d
StephanieAVT <stephanie.torisu08@gmail.com>
2020-09-23 16:23:40 -0300

    Alteracao Stephanie

--- src/com/br/digitalhouse/Colecao.kt
+++ src/com/br/digitalhouse/Colecao.kt
@@ -1,4 +1,10 @@
 package com.br.digitalhouse
 
-class Colecao {
+data class Colecao(val lista:MutableList<Livro>,val codigo:Int,val preco:Double,val descricao:String) {
+
+    fun verificaListaVazia(lista:MutableList<Livro>){
+        if(lista.isEmpty())
+            println("A lista está vazia ")
+    }
+    
 }
