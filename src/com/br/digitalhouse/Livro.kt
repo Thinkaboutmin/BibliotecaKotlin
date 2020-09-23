fa7f449b268f09ac55e56ddc2dd0c68525975aae
arthuraraujo550 <70539287+arthuraraujo550@users.noreply.github.com>
2020-09-23 19:59:37 -0300

    codigo tenso

--- src/com/br/digitalhouse/Livro.kt
+++ src/com/br/digitalhouse/Livro.kt
@@ -1,10 +1,9 @@
 package com.br.digitalhouse
 
 data class Livro(val codigo: Int, val autor: String, val anoDeLancamento: java.util.Date, val quantidadeEmEstoque: Int, val preco: Double, val titulo: String) {
-
-    fun verificarLivro(preco: Double, quantidadeEmEstoque: Int) {
-        if (preco < 0 || quantidadeEmEstoque < 0)
-            return "Digite valores válido"
+    fun validar() {
+        if (this.preco < 0 || this.quantidadeEmEstoque < 0)
+            return "Digite valores válidos"
 
     }
 }
