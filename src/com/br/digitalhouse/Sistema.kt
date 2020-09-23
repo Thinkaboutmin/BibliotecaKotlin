c6ec6a64a27b42d08474723cd72295753ea29eac
Michelle Soto <michelle.varas.soto@outlook.com>
2020-09-23 18:59:18 -0300

    sistema

--- src/com/br/digitalhouse/Sistema.kt
+++ src/com/br/digitalhouse/Sistema.kt
@@ -1,2 +1,39 @@
 package com.br.digitalhouse
 
+
+
+class Sistema(){
+
+    val livros =  mutableListOf<Livro>()
+    val colecoes =  mutableListOf<Colecao>()
+
+
+    fun cadastrarLivro(livro: Livro){
+        livros.add(livro)
+        println("Novo livro adicionado na lista")
+        println(livro)
+
+    }
+
+    fun cadastrarColecao(colecao: Colecao){
+        colecoes.add(colecao)
+        println("A colecao foi adicionada")
+        println(colecoes)
+    }
+
+    fun consultarLivro(codigo: Int){
+        println(livros.get(codigo))
+
+    }
+
+    fun consultarColecao(codigo: Int): Colecao{
+        println(livros[codigo])
+        return 0
+
+    }
+
+    fun efetuarVenda(codigo: Int): Int {
+        return 0
+
+    }
+}
