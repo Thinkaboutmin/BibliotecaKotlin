96ab33e68fa3d925afa18ee821de472d5792c483
arthuraraujo550 <70539287+arthuraraujo550@users.noreply.github.com>
2020-09-23 19:16:44 -0300

    alteracao arthur

--- src/com/br/digitalhouse/Livro.kt
+++ src/com/br/digitalhouse/Livro.kt
@@ -1,4 +1,10 @@
 package com.br.digitalhouse
 
-class Livro {
+data class Livro(val codigo: Int, val autor: String, val anoDeLancamento: java.util.Date, val quantidadeEmEstoque: Int, val preco: Double, val titulo: String) {
+
+    fun verificarLivro(preco: Double, quantidadeEmEstoque: Int) {
+        if (preco < 0 || quantidadeEmEstoque < 0)
+            return "Digite valores válido"
+
+    }
 }
